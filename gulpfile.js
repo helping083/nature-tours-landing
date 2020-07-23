afc0d6b8f68aee218b215e4a6f5d9d6879747b62
helping083 <helping083@gmail.com>
2020-07-23 13:04:41 +0300

    add styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 const gulp = require('gulp');
-
+let browserSync = require("browser-sync").create();
 gulp.task('hello', function() {
   console.log('Hello Zell');
+});
+gulp.task('serve', function() {
+  browserSync.init({
+    server: "./"
+  });
 });
